6d299547ed64ae096a326eae5e27a39d66a292f9
Kate <murraykeaton422@gmail.com>
2024-01-12 06:17:09 -0500

    created controller to load in index page, created blade layout for index

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', FrontController::class, 'index'); 
+Route::get('/', [FrontController::class, 'index']); 
