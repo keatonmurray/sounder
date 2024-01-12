37eb1375630d1c731063f98e42efe7e33d0ec923
Kate <murraykeaton422@gmail.com>
2024-01-12 07:29:36 -0500

    downloaded bootstrap and css plugins, created frontend

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -15,14 +15,11 @@
     <!--<link rel="icon" href="img/core-img/favicon.ico">-->
 
     <!-- Stylesheet -->
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
 
-    <!-- Bootstrap CDN --> 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
     <body>
-        <!-- Preloader -->
+        <!-- Preloader 
         <div class="preloader d-flex align-items-center justify-content-center">
             <div class="lds-ellipsis">
                 <div></div>
@@ -30,7 +27,7 @@
                 <div></div>
                 <div></div>
             </div>
-        </div>
+        </div>-->
 
     <!-- ##### Header Area Start ##### -->
     <header class="header-area">
@@ -42,7 +39,7 @@
                     <nav class="classy-navbar justify-content-between" id="oneMusicNav">
 
                         <!-- Nav brand -->
-                        <a href="index.html" class="nav-brand"><img src="img/core-img/logo.png" alt=""></a>
+                        <a href="index.html" class="nav-brand"><img src="{{asset('assets/img/core-img/logo.png')}}" alt=""></a>
 
                         <!-- Navbar Toggler -->
                         <div class="classy-navbar-toggler">
@@ -117,6 +114,40 @@
         </div>
     </header>
     <!-- ##### Header Area End ##### -->
+
+    <!-- ##### Hero Area Start ##### -->
+    <section class="hero-area">
+        <div class="hero-slides owl-carousel">
+            <!-- Single Hero Slide -->
+            <div class="single-hero-slide d-flex align-items-center justify-content-center">
+                <!-- Slide Img -->
+                <div class="slide-img bg-img" style="background-image: url({{asset('assets/img/bg-img/bg-1.jpg')}});"></div>
+                <!-- Slide Content -->
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="hero-slides-content text-center">
+                                <h6 data-animation="fadeInUp" data-delay="100ms">Latest album</h6>
+                                <h2 data-animation="fadeInUp" data-delay="300ms">Beyond Time <span>Beyond Time</span></h2>
+                                <a data-animation="fadeInUp" data-delay="500ms" href="#" class="btn oneMusic-btn mt-50">Discover <i class="fa fa-angle-double-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    <!-- ##### All Javascript Script ##### -->
+    <!-- jQuery-2.2.4 js -->
+    <script src="{{asset('assets/js/jquery/jquery-2.2.4.min.js')}}"></script>
+    <!-- Popper js -->
+    <script src="{{asset('assets/js/bootstrap/popper.min.js')}}"></script>
+    <!-- Bootstrap js -->
+    <script src="{{asset('assets/js/bootstrap/bootstrap.min.js')}}"></script>
+    <!-- All Plugins js -->
+    <script src="{{asset('assets/js/plugins/plugins.js')}}"></script>
+   <!-- Active js -->
+   <script src="{{asset('assets/js/active.js')}}"></script>
     </body>
 
 </head>
