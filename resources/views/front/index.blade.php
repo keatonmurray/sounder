cbb4adc4195ee9cb7bb4b361651fb05f34e2bc6d
Kate <murraykeaton422@gmail.com>
2024-01-12 07:32:38 -0500

    created slideshow bg image

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -135,6 +135,24 @@
                     </div>
                 </div>
             </div>
+             <!-- Single Hero Slide -->
+             <div class="single-hero-slide d-flex align-items-center justify-content-center">
+                <!-- Slide Img -->
+                <div class="slide-img bg-img" style="background-image: url({{asset('assets/img/bg-img/bg-2.jpg')}});"></div>
+                <!-- Slide Content -->
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="hero-slides-content text-center">
+                                <h6 data-animation="fadeInUp" data-delay="100ms">Latest album</h6>
+                                <h2 data-animation="fadeInUp" data-delay="300ms">Colorlib Music <span>Colorlib Music</span></h2>
+                                <a data-animation="fadeInUp" data-delay="500ms" href="#" class="btn oneMusic-btn mt-50">Discover <i class="fa fa-angle-double-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         </section>
 
     <!-- ##### All Javascript Script ##### -->
