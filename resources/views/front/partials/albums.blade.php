4e93868125d06672e6409fa53649db2a2a289f38
Kate <murraykeaton422@gmail.com>
2024-01-14 03:03:21 -0500

    db results are now pulled from the db and displayed in the albums page instead of static htmls

--- resources/views/front/partials/albums.blade.php
+++ resources/views/front/partials/albums.blade.php
@@ -11,90 +11,26 @@
         <div class="row justify-content-center">
             <div class="col-12 col-lg-9">
                 <div class="ablums-text text-center mb-70">
-                    <p>Organic gastropub affogato narwhal biodiesel four loko. Slow-carb bespoke hexagon ugh fit letterpress. Mollit glossier intelligentsia, kombucha PBR&B skateboard bitters adaptogen everyday carry tonx scenester wolf minim stumptown poke. Lumbersexual cillum raw denim duis, quis pop-up crucifix.</p>
+                    <p>Nam tristique ex vel magna tincidunt, ut porta nisl finibus. Vivamus eu dolor eu quam varius rutrum. Fusce nec justo id sem aliquam fringilla nec non lacus. Suspendisse eget lobortis nisi, ac cursus odio. Vivamus nibh velit, rutrum at ipsum ac, dignissim iaculis ante. Donec in velit non elit pulvinar pellentesque et non eros.</p>
                 </div>
             </div>
         </div>
-
         <div class="row">
             <div class="col-12">
                 <div class="albums-slideshow owl-carousel">
-                    <!-- Single Album -->
-                    <div class="single-album">
-                        <img src="{{asset('assets/img/bg-img/a1.jpg')}}" alt="">
-                        <div class="album-info">
-                            <a href="/show">
-                                <h5>The Cure</h5>
-                            </a>
-                            <p>Second Song</p>
-                        </div>
-                    </div>
-
-                    <!-- Single Album -->
-                    <div class="single-album">
-                        <img src="{{asset('assets/img/bg-img/a2.jpg')}}" alt="">
-                        <div class="album-info">
-                            <a href="/show">
-                                <h5>Sam Smith</h5>
-                            </a>
-                            <p>Underground</p>
-                        </div>
-                    </div>
-
-                    <!-- Single Album -->
-                    <div class="single-album">
-                        <img src="{{asset('assets/img/bg-img/a3.jpg')}}" alt="">
-                        <div class="album-info">
-                            <a href="/show">
-                                <h5>Will I am</h5>
-                            </a>
-                            <p>First</p>
-                        </div>
-                    </div>
-
-                    <!-- Single Album -->
-                    <div class="single-album">
-                        <img src="{{asset('assets/img/bg-img/a4.jpg')}}" alt="">
-                        <div class="album-info">
-                            <a href="/show">
-                                <h5>The Cure</h5>
-                            </a>
-                            <p>Second Song</p>
-                        </div>
-                    </div>
-
-                    <!-- Single Album -->
-                    <div class="single-album">
-                        <img src="{{asset('assets/img/bg-img/a5.jpg')}}" alt="">
-                        <div class="album-info">
-                            <a href="/show">
-                                <h5>DJ SMITH</h5>
-                            </a>
-                            <p>The Album</p>
-                        </div>
-                    </div>
-
-                    <!-- Single Album -->
-                    <div class="single-album">
-                        <img src="{{asset('assets/img/bg-img/a6.jpg')}}" alt="">
-                        <div class="album-info">
-                            <a href="/show">
-                                <h5>The Ustopable</h5>
-                            </a>
-                            <p>Unplugged</p>
-                        </div>
-                    </div>
-
-                    <!-- Single Album -->
-                    <div class="single-album">
-                        <img src="{{asset('assets/img/bg-img/a7.jpg')}}" alt="">
-                        <div class="album-info">
-                            <a href="/show">
-                                <h5>Beyonce</h5>
-                            </a>
-                            <p>Songs</p>
-                        </div>
-                    </div>
+                    @if(count($results) > 0)
+                        @foreach($results as $music)
+                            <div class="single-album">
+                                <img src="{{asset('/assets/img/bg-img/a1.jpg')}}" alt="">
+                                <div class="album-info">
+                                    <a href="/albums/{{$music->id}}">
+                                        <h5>{{$music->artist_name}}</h5>
+                                    </a>
+                                    <p>{{$music->album_name}}</p>
+                                </div>
+                            </div>
+                        @endforeach
+                    @endif
                 </div>
             </div>
         </div>
