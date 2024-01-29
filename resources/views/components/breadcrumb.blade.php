2fac39542a85d34edda52e2f10e0f53b3db05cb7
Kate <murraykeaton422@gmail.com>
2024-01-29 02:16:57 -0500

    styled account registration page

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -1,4 +1,4 @@
-<section class="breadcumb-area bg-img bg-overlay" style="background-image: url({{asset('assets/img/bg-img/breadcumb3.jpg')}});">
+<section class="breadcumb-area bg-img bg-overlay" style="background-image: url({{asset('assets/img/bg-img/vaporwave.jpg')}});">
     <div class="bradcumbContent">
         {{$slot}}
     </div>
