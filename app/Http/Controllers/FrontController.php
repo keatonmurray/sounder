9a2c99b5d07dc9bd6db443b9ae3662fd24c0f827
Kate <murraykeaton422@gmail.com>
2024-02-02 06:53:47 -0500

    everything works

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -7,12 +7,6 @@ use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
-
-    
-    public function __construct()
-    {
-        $this->middleware(['auth'], ['except' => ['index', 'show']]);
-    }
     
 
     public function index() 
