839bf47202f5874ec479e0c4fb97584d219cba88
Kate <murraykeaton422@gmail.com>
2024-02-05 09:35:59 -0500

    updated stylesheet

--- resources/views/front/create.blade.php
+++ resources/views/front/create.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 @section('content')
+<div class="head py-5 bg-dark"><br></div>
     <section class="upload-music-area bg-white">
       <form action="/store" method="POST" enctype="multipart/form-data">
         <div class="container my-5" style="height: auto;">
               <div class="hero section-padding-30">
-                <br><br><br><br><br>
                   @csrf
                     <x-input-fields/>
                 </div>
