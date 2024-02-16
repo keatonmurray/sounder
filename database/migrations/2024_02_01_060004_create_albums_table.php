845866f661267b6750b520adf8f9a30e8b10ea96
Kate <murraykeaton422@gmail.com>
2024-02-16 01:44:20 -0500

    migrated merches table to sounder db

--- database/migrations/2024_02_01_060004_create_albums_table.php
+++ database/migrations/2024_02_01_060004_create_albums_table.php
@@ -23,24 +23,6 @@ return new class extends Migration
             $table->integer('digital_audio_price');
             $table->integer('single_track_price');
             $table->integer('play_limit');
-            /*
-            $table->string('merch_first_upload');
-            $table->string('merch_second_upload');
-            $table->string('merch_third_upload');
-            $table->string('merch_fourth_upload');
-            $table->string('merch_name');
-            $table->integer('merch_price');
-            $table->longText('merch_description');
-            $table->string('ship_from');
-            $table->string('ship_to');
-            $table->string('city');
-            $table->string('zip_code');
-            $table->integer('shipping_cost');
-            $table->string('shipping_currency');
-            $table->string('vat_country');
-            $table->integer('vat_estimate');
-            $table->string('vat_currency');
-            */
             $table->timestamps();
         });
     }
---
+++ database/migrations/2024_02_16_064305_create_merches_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('merches', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('merch_first_upload');
+            $table->string('merch_second_upload');
+            $table->string('merch_third_upload');
+            $table->string('merch_fourth_upload');
+            $table->string('merch_name');
+            $table->integer('merch_price');
+            $table->longText('merch_description');
+            $table->string('ship_from');
+            $table->string('ship_to');
+            $table->string('city');
+            $table->string('zip_code');
+            $table->integer('shipping_cost');
+            $table->string('shipping_currency');
+            $table->string('vat_country');
+            $table->integer('vat_estimate');
+            $table->string('vat_currency');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('merches');
+    }
+};
