69beea7d5af978191395b9f090108a31cbf0bfb5
Kate <murraykeaton422@gmail.com>
2024-02-19 03:16:01 -0500

    changed nav fill color

--- resources/views/artist/partials/follow-count.blade.php
+++ resources/views/artist/partials/follow-count.blade.php
@@ -14,11 +14,3 @@
         </div>
     </div>
 </div>
-<div class="card" style="border-width: 0;">
-    <div class="col">
-        <div class="card-body">
-            <h5 class="card-title">Following</h5>
-            <h6 class="card-subtitle mb-2">49</h6>
-        </div>
-    </div>
-</div>
