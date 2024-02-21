caa10775ae5ea4b2832687c3324618cf16cfbf1f
Kate <murraykeaton422@gmail.com>
2024-02-21 03:45:18 -0500

    styled edit profile page

--- resources/views/artist/profile/profile-settings.blade.php
+++ resources/views/artist/profile/profile-settings.blade.php
@@ -1,4 +1,52 @@
 @extends('layouts.app')
 @section('content')
-
+    <div class="head py-5 bg-dark"><br></div>
+    <section id="profileSettingsArea" class="py-2" style="height: auto; background: #f5f9fa;">
+        <div class="container my-4">
+            <x-form.body>
+                <x-form.heading heading="Profile Settings"/>
+                <br>
+                <form action="" method="POST">
+                    <div class="row">
+                        <div class="col-12 col-sm-6">
+                            <x-form.border>
+                                <div class="text-center mb-4 mx-2">
+                                  <h6>Profile Picture</h6>
+                                    <x-form.thumbnail type="mt-3 mb-2">
+                                      <img src="{{asset('assets/img/bg-img/gallery.svg')}}" class="avatar" alt="" style="height: 200px; width: 200px;">
+                                    </x-form.thumbnail>
+                                    <p>Profile photo mustt not exceed 600x600 pixels</p>
+                                    <input type="file" name="album_cover" id="file" class="file-upload">
+                                    <label for="file" class="btn btn-dark d-block">Choose a file</label>
+                                </div>
+                            </x-form.border><br/>
+                        </div>
+                        <div class="col-12 col-sm-6">
+                            <x-form.border>
+                                <div class="text-center mb-4 mx-2">
+                                  <h6>Cover Photo</h6>
+                                    <x-form.thumbnail type="mt-3 mb-2">
+                                      <img src="{{asset('assets/img/bg-img/gallery.svg')}}" class="avatar" alt="" style="height: 200px; width: 200px;">
+                                    </x-form.thumbnail>
+                                    <p>Cover photo must not exceed 1200x1200 pixels</p>
+                                    <input type="file" name="album_cover" id="file" class="file-upload">
+                                    <label for="file" class="btn btn-dark d-block">Choose a file</label>
+                                </div>
+                            </x-form.border><br/>
+                        </div>
+                        <div class="col-12 mt-30">
+                            <x-form.heading heading="Profile Information"/>
+                            <div class="mb-3">
+                                <label for="profile_name" class="form-label">Account Name</label>
+                                <input type="text" name="profile_name" class="form-control" id="profile_name" placeholder="Enter an artist name">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="text-center mt-30">
+                        <button class="btn oneMusic-btn-dark d-block w-100" type="submit">Update Profile</button>
+                      </div>
+                </form>
+            </x-form.body>
+        </div>
+    </section>
 @endsection
