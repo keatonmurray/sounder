a3fcd48a158c482ec6b70e43a818503bc9d7713b
Kate <murraykeaton422@gmail.com>
2024-03-04 01:36:11 -0500

    renamed model to ArtistProfile and created associated Controller

--- app/Models/ArtistProfile.php
+++ app/Models/ArtistProfile.php
@@ -6,7 +6,7 @@ use App\Models\Artist;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Profile extends Model
+class ArtistProfile extends Model
 {
     use HasFactory;
 
