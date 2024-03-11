d9410bbfd2f9c8b9f4cd73eac41a1eec30956c63
Kate <murraykeaton422@gmail.com>
2024-03-11 00:52:57 -0400

    added edit profile and share profile buttons

--- resources/views/fan/authenticated/auth.blade.php
+++ resources/views/fan/authenticated/auth.blade.php
@@ -1,17 +1,17 @@
 <section id="artistProfile1" class="jumbotron-fluid text-center border-0 bg-white">
-    <div class="head px-5 py-4 bg-absolute bg-overlay justify-content-center" style="background-image: url('{{$profile->profile_picture ? asset('storage/' . $profile->cover_photo) : asset('assets/img/bg-img/no-image.png')}}');
+    <div class="head px-5 py-4 bg-absolute bg-overlay justify-content-center" style="background-image: url('{{asset('assets/img/bg-img/no-image.png')}}');
         height: auto;">
         <div class="artist-profile-section">
             <div id="artistProfileInfo">
-                <img src="{{$profile->profile_picture ? asset('storage/' . $profile->profile_picture) : asset('assets/img/bg-img/no-image.png')}}" class="rounded-circle" ></br>
-                <h2 class="artist-name">{{$profile->artists->name}}</h2>
+                <img src="{{ asset('assets/img/bg-img/no-image.png')}}" class="rounded-circle" ></br>
+                <h2 class="artist-name">John Doe</h2>
                 <h6 class="country">Montreal, CA</h6>
                 <div class="buttons">
-                    <a href="/update-artist-profile/{{$profile->id}}" id="followBtn" class="btn btn-light mt-2 mr-1">
+                    <a href="" id="followBtn" class="btn btn-light mt-2 mr-1">
                         <i class="fa-solid fa-user-pen"></i> Edit Profile
                     </a>
-                    <a href="/artist-dashboard" id="messageBtn" class="btn btn-light mt-2 ml-1">
-                        <i class="fa-solid fa-user"></i> Go to Dashboard
+                    <a href="/" id="messageBtn" class="btn btn-light mt-2 ml-1">
+                        <i class="fa-solid fa-share"></i> Share Profile
                     </a>
                 </div>
             </div>
@@ -26,13 +26,13 @@
             </div>
             <div class="tab-content mt-30" id="nav-tabContent">
                 <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-                   @include('artist.auth.partials.albums')
+                  
                 </div>
                 <div class="tab-pane fade" id="nav-merch" role="tabpanel" aria-labelledby="nav-merch-tab">
-                    @include('artist.auth.partials.merch')
+                   
                 </div>
                 <div class="tab-pane fade" id="nav-tours" role="tabpanel" aria-labelledby="nav-tours-tab">
-                    @include('artist.auth.partials.tours')
+                   
                 </div>
             </div>
         </div>
