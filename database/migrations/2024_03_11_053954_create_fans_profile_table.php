7d2934e21cdb1a2599888fc8624d7245c852525d
Kate <murraykeaton422@gmail.com>
2024-03-11 01:41:02 -0400

    migrated table

--- database/migrations/2024_03_11_053954_create_fans_profile_table.php
+++ database/migrations/2024_03_11_053954_create_fans_profile_table.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('fans_profile', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('email');
+            $table->string('profile_picture');
+            $table->string('cover_photo');
             $table->timestamps();
         });
     }
