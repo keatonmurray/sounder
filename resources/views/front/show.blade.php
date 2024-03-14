e18874bba297c232409fccf76bd3d91d51793834
Kate <murraykeaton422@gmail.com>
2024-03-14 01:39:30 -0400

    wrapped the add to wishlist button inside form tags

--- resources/views/front/show.blade.php
+++ resources/views/front/show.blade.php
@@ -5,70 +5,70 @@
             <!--image here -->
         </div>
         <div class="container my-4">  
-            <form action="" method="POST">
-                @csrf
-                <div class="row align-items-end">
-                    <div class="col-12 col-md-5 col-lg-4">
-                        <div class="container text-center">
-                            <div class="featured-artist-thumb">
-                                <img src="{{asset('storage/' . $albums->album_cover)}}">
-                            </div>
-                            <div class="d-flex justify-content-center mt-3">
-                                <a href="" class="btn btn-light mr-2 w-100 font-weight-bold" type="button">
-                                    <i class="fa-brands fa-paypal mr-1"></i>
-                                    Buy for ${{$albums->digital_audio_price}}
-                                </a>
-                                <a href="" class="btn btn-light ml-2 w-100 font-weight-bold" type="submit">
+            <div class="row align-items-end">
+                <div class="col-12 col-md-5 col-lg-4">
+                    <div class="container text-center">
+                        <div class="featured-artist-thumb">
+                            <img src="{{asset('storage/' . $albums->album_cover)}}">
+                        </div>
+                        <div class="d-flex justify-content-center mt-3">
+                            <a href="" class="btn btn-light mr-2 w-100 font-weight-bold" type="button">
+                                <i class="fa-brands fa-paypal mr-1"></i>
+                                Buy for ${{$albums->digital_audio_price}}
+                            </a>
+                            <form action="" method="POST">
+                                <button class="btn btn-light ml-2 w-100 font-weight-bold" type="submit">
                                     <i class="fa-solid fa-heart mr-1"></i>
                                     Add to wishlist
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-7 col-lg-8">
-                        <div class="featured-artist-content">
-                            <div class="section-heading white text-left mb-30">
-                                <p>{{$albums->artists->name}}</p>
-                                <h2>{{$albums->album_title}}</h2>
-                            </div>
-                            <div class="song-play-area">
-                                <div class="song-name">
-                                    <p class="text-white">{{Str::between($albums->audios[0], '/', '.')}}</p>
-                                </div>
-                                <audio preload="auto" controls>
-                                    <source src="{{asset('storage/' . $albums->audios[0])}}" type="audio/mp3">
-                                </audio>
-                            </div>
-                            <div class="container">
-                                <button class="btn btn-light d-block w-100 font-weight-bold">
-                                    <i class="fa-solid fa-plus"></i>
-                                    Follow {{$albums->artists->name}}
                                 </button>
-                            </div> <br><br><br>
-                            <div class="container mt-2">
-                                <p>{{$albums->album_description}}</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="discographies mt-30 col-12 col-md-12 col-lg-8">
-                        <div class="song-play-area">
-                            <h4 class="text-white mb-4">All Songs</h4>
-                            @foreach($albums->audios as $result)
-                                <div class="song-name">
-                                    <button class="btn btn-light btn-sm ml-30 float-right font-weight-bold">
-                                        <i class="fa-brands fa-paypal mr-1"></i>
-                                        Buy for ${{$albums->single_track_price}}</button>
-                                    <p class="text-white">{{Str::between($result, '/', '.')}}</p>
-                                </div>
-                                <audio preload="auto" controls>
-                                    <source src="{{asset('storage/' . $result)}}" type="audio/mp3">
-                                </audio>
-                                <br>
-                            @endforeach
+                            </form>
                         </div>
                     </div>
                 </div>
-                @if($merch)
+                <div class="col-12 col-md-7 col-lg-8">
+                    <div class="featured-artist-content">
+                        <div class="section-heading white text-left mb-30">
+                            <p>{{$albums->artists->name}}</p>
+                            <h2>{{$albums->album_title}}</h2>
+                        </div>
+                        <div class="song-play-area">
+                            <div class="song-name">
+                                <p class="text-white">{{Str::between($albums->audios[0], '/', '.')}}</p>
+                            </div>
+                            <audio preload="auto" controls>
+                                <source src="{{asset('storage/' . $albums->audios[0])}}" type="audio/mp3">
+                            </audio>
+                        </div>
+                        <div class="container">
+                            <button class="btn btn-light d-block w-100 font-weight-bold">
+                                <i class="fa-solid fa-plus"></i>
+                                Follow {{$albums->artists->name}}
+                            </button>
+                        </div> <br><br><br>
+                        <div class="container mt-2">
+                            <p>{{$albums->album_description}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="discographies mt-30 col-12 col-md-12 col-lg-8">
+                    <div class="song-play-area">
+                        <h4 class="text-white mb-4">All Songs</h4>
+                        @foreach($albums->audios as $result)
+                            <div class="song-name">
+                                <button class="btn btn-light btn-sm ml-30 float-right font-weight-bold">
+                                    <i class="fa-brands fa-paypal mr-1"></i>
+                                    Buy for ${{$albums->single_track_price}}</button>
+                                <p class="text-white">{{Str::between($result, '/', '.')}}</p>
+                            </div>
+                            <audio preload="auto" controls>
+                                <source src="{{asset('storage/' . $result)}}" type="audio/mp3">
+                            </audio>
+                            <br>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+            @if($merch)
                 <div class="discographies mt-30">
                     <div class="song-play-area">
                         <h4 class="text-white mb-4">Buy Merch</h4>
@@ -88,9 +88,8 @@
                             </div>
                         </div>
                     </div>   
-                @endif
                 </div>
-            </form>
+            @endif
         </div>
     </section>    
 @endsection
